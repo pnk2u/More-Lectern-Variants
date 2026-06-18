ca19068c723db911d5ee1052aac3d1b8da697fae
pnku <pnku@pnku.de>
2026-06-18 21:41:09 +0200

    Update to 26.2 (`1.9.3-26.1(2):c1`)

--- src/main/java/de/pnku/mlv/mixin/BlockEntityTypeMixin.java
+++ src/main/java/de/pnku/mlv/mixin/BlockEntityTypeMixin.java
@@ -1,8 +1,10 @@
 package de.pnku.mlv.mixin;
 
 import de.pnku.mlv.block.MoreLecternBlock;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class BlockEntityTypeMixin {
     @Inject(method = "isValid", at = @At("HEAD"), cancellable = true)
     private void isValid(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
-        if (BlockEntityType.LECTERN.equals(this) && blockState.getBlock() instanceof MoreLecternBlock) {
+        if (BuiltInRegistries.BLOCK_ENTITY_TYPE.get(Identifier.withDefaultNamespace("lectern")).get().value().equals(this) && blockState.getBlock() instanceof MoreLecternBlock) {
             cir.setReturnValue(true);
         }
     }
